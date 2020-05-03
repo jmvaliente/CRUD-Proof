71461c3a1c425199652a8c8e8dea7e6593a0e902
Jose Miguel Valiente Niño <valiente_jose2@hotmail.com>
2020-05-03 16:36:29 +0200

    login page

--- src/App.js
+++ src/App.js
@@ -4,16 +4,23 @@ import Products from './components/Products'
 import ProductCreate from './components/ProductCreate'
 import ProductEdit from './components/ProductEdit'
 
-import {Route, Switch} from 'react-router-dom'
+import Login from './components/auth/Login'
+
+import {Route, Switch, useHistory} from 'react-router-dom'
 
 import ProductState from './context/products/productState'
 
 function App() {
+  
+  const history = useHistory()
+  const headHide = history.location.pathname
+  
   return (
     <Fragment>
       <ProductState>
-      <Header/>
+        {headHide==='/login' ? null :<Header/>}
       <Switch>
+        <Route exact path = "/login" component = {Login}/>
         <Route exact path = "/" component = {Products}/>
         <Route exact path = "/product/create" component = {ProductCreate}/>
         <Route exact path = "/product/edit/:id" component = {ProductEdit}/>
